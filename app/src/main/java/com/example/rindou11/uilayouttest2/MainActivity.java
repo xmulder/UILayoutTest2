40bf8cdbfff8ce7b9fe33ae9f25d0ea989bb596c
Rindou.to <constantine.tu@gmail.com>
2018-08-28 11:56:39 +0800

    ..

--- app/src/main/java/com/example/rindou11/uilayouttest2/MainActivity.java
+++ app/src/main/java/com/example/rindou11/uilayouttest2/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button button_1;
+    private Button button_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         button_1=(Button)findViewById(R.id.button_1);
+        button_2=(Button)findViewById(R.id.button_2);
+
         button_1.setOnClickListener(this);
+        button_2.setOnClickListener(this);
     }
 
     public void onClick(View v){
@@ -24,6 +28,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_1:
                 Intent intent1=new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(intent1);
+                break;
+            case R.id.button_2:
+                Intent intent2=new Intent();
+                intent2.setAction("android.intent.layout.rlayout");
+                startActivity(intent2);
+
+            default:
+                break;
         }
     }
 
---
+++ app/src/main/java/com/example/rindou11/uilayouttest2/rlayout.java
@@ -0,0 +1,13 @@
+package com.example.rindou11.uilayouttest2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rlayout);
+    }
+}
